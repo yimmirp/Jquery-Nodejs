46ad4bbc6aac688b8c28f75817a3ea504c0f4284
ypernillo <yimmiss305@gmail.com>
2020-05-11 11:25:32 -0600

    Interaz terminada v1

--- src/index.js
+++ src/index.js
@@ -18,6 +18,31 @@ app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, 'views')));
 
 
+app.post('/compilar', (req, res) => {
+    const { code } = req.body;
+    console.log(code);
+    res.json({
+        code3D: `var t1,t2;
+var Stack[];
+var Heap[];
+var P = 0;
+var H = 0;
+if(2 < 3) goto L1;
+goto L2;
+L1:
+t1=5+0;
+print("%i" , t1);
+goto L3;
+L2:
+t2=10*1;
+print("%i" , t2);
+L3:`,
+        Console: 'Archivo compilado Exitosamente',
+        Errores: 'Error encontrado en la X linea',
+        Reportes: 'Report show in UI'
+    })
+})
+
 
 //run
 app.listen(app.get("port"), () => {
--- src/views/js/app.js
+++ src/views/js/app.js
@@ -1,140 +1,161 @@
+let countTab = 0;
+let idCodeArea = '';
+let idCode3D = '';
+let TitleCompiled = '';
+editors = []
+    //Estilos Consola
+var Consola = CodeMirror.fromTextArea(consola, {
+    lineNumbers: true,
+    theme: "ayu-dark"
+});
+Consola.setSize(null, 100);
 
+//Estilos Errores
+var Errores = CodeMirror.fromTextArea(errores, {
+    lineNumbers: true,
+    theme: "darcula"
+});
+Errores.setSize(null, 100);
 
-function addStyleArea(TextArea) {
+//Estilos Reporte
+var Reporte = CodeMirror.fromTextArea(reporte, {
+    lineNumbers: true,
+    theme: "nord"
+});
+Reporte.setSize(null, 100);
 
-    CodeMirror.fromTextArea(TextArea, {
+//Estilos Editor
+function addStyleArea(TextArea, content, idCodeArea) {
+    var editor = CodeMirror.fromTextArea(TextArea, {
         lineNumbers: true,
         theme: "mdn-like"
-
     });
+    editor.setSize(null, 370);
+    editor.setValue(content)
+    let editorA = {
+        id: idCodeArea,
+        meditor: editor
+    }
+    editors.push(editorA);
 }
 
-countTab = 0;
-// var Tabs = [
-//     {
-//         id: 1,
-//         tabName: "Untitled-1",
-//     }
-// ]
-
-
-// if (countTab > 0) {
-
-//     Tabs.forEach((tab) => {
-
-//         $('#myTab').append(`
-//         <li class="nav-item ">
-//         <a class="nav-link py-1 text-dark " id="${tab.id}" data-toggle="tab" href="#tabPane${tab.id}" role="tab"
-//             aria-controls="tabPane${tab.id}" >${tab.tabName} <span class="close ml-1">&times;</span>
-
-//         </a>
-//        </li>
-//         `)
-
-//         $('#tabPanels').append(`
-//         <div class="tab-pane border" id="tabPane${tab.id}" role="tabpanel" aria-labelledby="home-tab">
-//         <div class="row">
-//             <div class="col-7  pr-0">
-//                 <textarea name="codeArea${tab.id}" id="codeArea${tab.id}" cols="30" rows="10"></textarea>
-//             </div>
-//             <div class="col-5 pl-0">
-//                 <textarea name="c3d${tab.id}" id="c3d${tab.id}" cols="30" rows="10"></textarea>
-//             </div>
-//         </div>
-
-
-//        </div>
-
-//          `)
-
-//         let codeArea = document.getElementById('codeArea' + tab.id);
-//         let c3dArea = document.getElementById('c3d' + tab.id);
-
-//         addStyleArea(codeArea);
-//         addStyleArea(c3dArea);
-
-
-//     })
-
-
-// } else {
-
-//     $('#AreaCode').append(`
-//     <div class="row justify-content-center mt-5 pt-5">
-//     <div class="col-8 mt-5 pt-5 ">
-//     <h1>Carge un archivo o cree uno nuevo</h1>    
-
-//     </div>
-//     </div>
-//     `)
-
-
-// }
-
-function addNewTab(tabName) {
+//Agregar Nueva Tab
+function addNewTab(tabName, content) {
 
     countTab = countTab + 1;
     $('#myTab').append(`
-    <li class="nav-item ">
-    <a class="nav-link py-1 text-dark " id="${countTab}" data-toggle="tab" href="#tabPane${countTab}" role="tab"
-        aria-controls="tabPane${countTab}" >${tabName} <span class="close ml-1">&times;</span>
-    
-    </a>
+    <li class="nav-item">
+        <a class="nav-link py-1 text-dark" id="${countTab}" data-toggle="tab" href="#tabPane${countTab}" role="tab"
+            aria-controls="tabPane${countTab}">${tabName} <span class="close ml-1">&times;</span>
+        </a>
    </li>
     `)
 
     $('#tabPanels').append(`
-    <div class="tab-pane border" id="tabPane${countTab}" role="tabpanel" aria-labelledby="home-tab">
-    <div class="row">
-        <div class="col-7  pr-0">
-            <textarea name="codeArea${countTab}" id="codeArea${countTab}" cols="30" rows="10"></textarea>
+    <div class="tab-pane  fade show active" id="tabPane${countTab}" role="tabpanel" aria-labelledby="home-tab">
+        <div class="row">
+            <div class="col-7  ">
+                <textarea name="codeArea${countTab}" id="codeArea${countTab}" ></textarea>
+            </div>
+            <div class="col-5 ">
+                <textarea name="c3d${countTab}" id="c3d${countTab}" ></textarea>
+            </div>
         </div>
-        <div class="col-5 pl-0">
-            <textarea name="c3d${countTab}" id="c3d${countTab}" cols="30" rows="10"></textarea>
-        </div>
-    </div>
-   
-   
    </div>
     
      `)
 
-
     let codeArea = document.getElementById('codeArea' + countTab);
     let c3dArea = document.getElementById('c3d' + countTab);
-
-    addStyleArea(codeArea);
-    addStyleArea(c3dArea);
+    idCodeArea = 'codeArea' + countTab;
+    idCode3D = 'c3d' + countTab;
+    addStyleArea(codeArea, content, idCodeArea);
+    addStyleArea(c3dArea, "", idCode3D);
+    return countTab;
 }
 
-
-$(".nav-tabs").on("click", "span", function () {
+//CerrarTab
+$(".nav-tabs").on("click", "span", function() {
     var tab = $(this).parent();
     $(tab.attr("href")).remove();
-    $("#firstTab-nav").tab('show');
     tab.tab('dispose');
     tab.remove();
 });
 
-$('#myTab a').on('click', function (e) {
-    e.preventDefault()
-    console.log(this)
 
-})
-
-
-$('#btnCrear').on('click', function (e) {
+//Creando un Nuevo Archivo
+$('#btnCrear').on('click', function(e) {
 
     e.preventDefault()
     let title = $('#txtTitulo').val();
-    console.log(title)
     $('#txtTitulo').val('');
     $('#exampleModal').modal('hide')
-    addNewTab(title);
+    addNewTab(title, "");
     if (countTab == 1) {
         $('div').remove('#zero');
     }
     $("#" + countTab).tab('show');
+    TitleCompiled = title;
+})
+
+
+
+//Cargar Archivo
+const inputFile = document.querySelector('input[type="file"]');
+inputFile.addEventListener('change', function(e) {
+
+    title = inputFile.files[0]['name'];
+    const reader = new FileReader();
+    reader.onload = function() {
+        addNewTab(title, reader.result);
+        if (countTab == 1) {
+            $('div').remove('#zero');
+        }
+        $("#" + countTab).tab('show');
+
+        console.log(reader.result);
+    }
+
+    reader.readAsText(inputFile.files[0])
+    TitleCompiled = title;
+
+}, false)
+
+
+//TODO: Compilar
+$('#btnCompilar').on('click', function(e) {
+
+    e.preventDefault()
+    $('#CompileFile').text('Archivo Compilado: ' + TitleCompiled);
+    let codeArea = editors.find(editor => editor.id == idCodeArea);
+    let code3DArea = editors.find(editor => editor.id == idCode3D);
+
+    // console.log('ID TextArea: ' + idCodeArea);
+    // console.log('Title Text: ' + TitleCompiled);
+    // console.log(editors)
+    console.log(codeArea.meditor.getValue())
+    console.log(code3DArea.meditor.getValue())
+
+
+    // code3DArea.meditor.setValue('ddddd')
+
+    $.ajax({
+        url: '/compilar',
+        method: 'POST',
+        data: {
+            code: codeArea.meditor.getValue()
+        },
+        success: function(res) {
+            console.log(res);
+            code3DArea.meditor.setValue(res.code3D);
+            Consola.setValue(res.Console);
+            Errores.setValue(res.Errores);
+            Reporte.setValue(res.Reportes);
+        }
+    })
+
+
+
 
 })
 
@@ -142,3 +163,15 @@ $('#btnCrear').on('click', function (e) {
 
 
 
+
+//Foco de la Tab seleccionada
+$('#myTab').on('click', '.nav-link', function(e) {
+    e.preventDefault()
+    let name = $(this).text().split(' ')
+    idCodeArea = 'codeArea' + this.id;
+    idCode3D = 'c3d' + this.id;
+
+    TitleCompiled = name[0];
+
+
+})
